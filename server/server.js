2f6ce3342ad3d422af2a739fc90317ec6e19e831
Peyton Lao <peyton.lao@uconn.edu>
2024-07-12 13:09:37 -0400

    server test

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { promises as fs } from 'fs';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
+
+const app = express();
+app.use(cors());
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/socks', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const socks = await collection.find({}).toArray();
+        res.json(socks);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
