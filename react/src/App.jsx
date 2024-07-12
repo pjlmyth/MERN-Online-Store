35efd077c30fee7a09753ad8c806dbb6aaad91ce
Megan Sidmore <msidmore@travelers.com>
2024-07-12 14:22:46 -0400

    handbags

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,15 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar'
+import Handbags from './components/Handbags'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/product`);
+            if (!response.ok) {
+                throw new Error('Data could not be fetched!');
+            }
+            const json_response = await response.json();
+            setData(json_response); // assign JSON response to the data variable.
+        } catch (error) {
+            console.error('Error fetching socks:', error);
+        }
+    };
+
+    fetchData();
+}, []);
 
   return (
     <>
+    <Router>
       <NavBar />
+      <Routes>
+          <Route exact path="/handbags" element={<Handbags data={data} />} />
+          
+      </Routes>
+    </Router>
+      
     </>
   )
 }
--- react/src/components/Handbags.jsx
+++ react/src/components/Handbags.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Handbags = (props) => {
     return (
-        <></>
+        <>
+        <p>Hello World</p>
+        </>
     );
 };
 
