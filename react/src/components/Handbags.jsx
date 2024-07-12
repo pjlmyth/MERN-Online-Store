07ce51e3f696d1e868654a0b1bfce9cb84791d9f
Aarti Arasada <aarti.arasada123@gmail.com>
2024-07-12 14:00:01 -0400

    fe

---
+++ react/src/components/Handbags.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Handbags = (props) => {
+    return (
+        <></>
+    );
+};
+
+export default Handbags;
--- react/src/components/NavBar.jsx
+++ react/src/components/NavBar.jsx
@@ -25,10 +25,12 @@ const NavBar = (props) => {
                   More
                 </a>
                 <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><a className="dropdown-item" href="#">Handbags</a></li>
+                  <li><a className="dropdown-item" href="#">Jewelry</a></li>
+                  <li><a className="dropdown-item" href="#">Shoes</a></li>
+                  <li><a className="dropdown-item" href="#">Perfumes</a></li>
                   <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  <li><a className="dropdown-item" href="#">Account Info</a></li>
                 </ul>
               </li>
             </ul>
